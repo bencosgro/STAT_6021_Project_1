db0f8f150631f267abc9e07e5ad3c173bc85e475
Ben Cosgro <bencosgro@gmail.com>
2020-07-24 14:44:25 -0400

    halp

--- DiamondRfile.R
+++ DiamondRfile.R
@@ -12,12 +12,12 @@ table(data$color)
 table(data$clarity)
 table(data$cut)
 
-data$cutscore <- revalue(data$cut, c("Good"="0", "Very Good"="1", "Ideal"="2", "Astor Ideal"="3"))
+data$colorscore <- revalue(data$cut, c("J"="0", "I"="1", "H"="2", "G"="3", "F"="4", "E"="5", "D"="6"))
 data$clarityscore <- revalue(data$clarity, c("SI2"="0", "SI1"="1", "VS2"="2", "VS1"="3","VVS2"="4", "VVS1"="5", "IF"="6", "FL"="7"))
 
 attach(data)
 
-data$cutscore <- as.numeric(cutscore)
+data$colorscore <- as.numeric(colorscore)
 data$clarityscore <- as.numeric(clarityscore)
 plot(cutscore~clarityscore)
 
